3c7b59ffd11e8ef42638f0c958746e9662567334
ella <yanzi19841227@gmail.com>
2024-01-22 21:01:09 +0800

    新增 footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,26 +1,66 @@
 <script setup lang="ts">
-import { logo } from "@/data/ImagePaths";
+import { logo, LINE, IG } from "@/data/ImagePaths";
 const { VITE_COMPANY_NAME } = import.meta.env; 
-// import { ref } from 'vue'
-
-// defineProps<{ msg: string }>()
-
-// const count = ref(0)
 </script>
 
 <template>
-  FooterComponent
+    <div class="container-lg pt-12 pb-12 pb-md-14 text-white d-grid gap-12">
+        <div class="row row-cols-1 row-cols-md-2 justify-content-between d-grid gap-8">
+            <div class="col d-grid gap-8">
+                <img :src="logo" :alt="VITE_COMPANY_NAME" class="logo">
+                <div class="d-flex align-items-center gap-4">
+                    <a href="#">
+                        <img :src="LINE" alt="LINE" class="community-img">
+                    </a>
+                    <a href="#">
+                        <img :src="IG" alt="IG" class="community-img">
+                    </a>
+                </div>
+            </div>
+            <div class="col d-flex gap-10">
+                <ul class="d-grid gap-4 gap-md-8 list-unstyled mb-0">
+                    <li>
+                        <ul class="d-grid d-md-flex align-items-center gap-4 gap-md-12 list-unstyled mb-0">
+                            <li class="d-grid gap-2">
+                                <strong class="d-block text-title">TEL</strong>
+                                <a href="tel:+886-7-1234567" class="link-neutral-0 text-decoration-none fw-normal">+886-7-1234567</a>
+                            </li>
+                            <li class="d-grid gap-2">
+                                <strong class="d-block text-title">MAIL</strong>
+                                <a href="mailto:elh@hexschool.com" class="link-neutral-0 text-decoration-none fw-normal">elh@hexschool.com</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul class="d-grid d-md-flex align-items-center gap-4 gap-md-12 list-unstyled mb-0">
+                            <li class="d-grid gap-2">
+                                <strong class="d-block text-title">FAX</strong>
+                                <span>+886-7-1234567</span>
+                            </li>
+                            <li class="d-grid gap-2">
+                                <strong class="d-block text-title">MAIL</strong>
+                                <a href="www.elhhexschool.com.tw" class="link-neutral-0 text-decoration-none fw-normal">www.elhhexschool.com.tw</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 d-grid gap-4">
+            <div class="col">806023 台灣高雄市新興區六角路123號</div>
+            <div class="col text-md-end">© 享樂酒店 2023 All Rights Reserved.</div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
-.navbar-brand {
-  img {
-    width: 100%;
-    height: 40px;
+img.logo {
+    width: auto;
+    height: 72px;
+}
 
-    @include media-breakpoint-up(lg) {
-      height: 72px;
-    }
-  }
+img.community-img {
+    width: 40px;
+    height: 40px;
 }
 </style>
--- src/data/imagePaths.ts
+++ src/data/imagePaths.ts
@@ -3,11 +3,15 @@ import hero from "@/assets/images/hero.png";
 import line3 from "@/assets/images/line3.png";
 import register from "@/assets/images/register.png";
 import user1 from "@/assets/images/register.png";
+import LINE from "@/assets/images/LINE.svg";
+import IG from "@/assets/images/IG.svg";
 
 export {
     logo,
     hero,
     line3,
     register,
-    user1
+    user1,
+    LINE,
+    IG
 }
