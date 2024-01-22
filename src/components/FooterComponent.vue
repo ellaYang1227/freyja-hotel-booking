0ebb6cc861bc9a6c23c8ed60f86197cb3ebeca45
ella <yanzi19841227@gmail.com>
2024-01-22 21:11:13 +0800

    調整 md 以上畫面排版 bug

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,7 +5,7 @@ const { VITE_COMPANY_NAME } = import.meta.env;
 
 <template>
     <div class="container-lg pt-12 pb-12 pb-md-14 text-white d-grid gap-12">
-        <div class="row row-cols-1 row-cols-md-2 justify-content-between d-grid gap-8">
+        <div class="row row-cols-1 row-cols-md-2 justify-content-between d-grid d-md-flex gap-8 gap-md-0">
             <div class="col d-grid gap-8">
                 <img :src="logo" :alt="VITE_COMPANY_NAME" class="logo">
                 <div class="d-flex align-items-center gap-4">
@@ -17,7 +17,7 @@ const { VITE_COMPANY_NAME } = import.meta.env;
                     </a>
                 </div>
             </div>
-            <div class="col d-flex gap-10">
+            <div class="col d-flex justify-content-end gap-10">
                 <ul class="d-grid gap-4 gap-md-8 list-unstyled mb-0">
                     <li>
                         <ul class="d-grid d-md-flex align-items-center gap-4 gap-md-12 list-unstyled mb-0">
@@ -46,7 +46,7 @@ const { VITE_COMPANY_NAME } = import.meta.env;
                 </ul>
             </div>
         </div>
-        <div class="row row-cols-1 row-cols-md-2 d-grid gap-4">
+        <div class="row row-cols-1 row-cols-md-2 d-grid d-md-flex gap-4 gap-md-0">
             <div class="col">806023 台灣高雄市新興區六角路123號</div>
             <div class="col text-md-end">© 享樂酒店 2023 All Rights Reserved.</div>
         </div>
