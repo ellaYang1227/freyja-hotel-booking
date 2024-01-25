a5fd114726fccb2e005b3c1a3a21c065637d241d
ellayang <ellayang@mail.apex.com.tw>
2024-01-25 15:16:07 +0800

    調整 footer、移除手機版 body font-size

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -6,8 +6,8 @@ const { VITE_COMPANY_NAME } = import.meta.env;
 <template>
     <img :src="line_mobile" :alt="VITE_COMPANY_NAME" class="line-img d-lg-none">
     <img :src="line2" :alt="VITE_COMPANY_NAME" class="line-img d-none d-lg-block">
-    <div class="container-lg pt-12 pb-12 pb-md-14 text-white d-grid gap-12">
-        <div class="row row-cols-1 row-cols-md-2 justify-content-between d-grid d-md-flex gap-8 gap-md-0">
+    <div class="container-lg pt-12 pb-12 pb-lg-14 text-white d-grid gap-12">
+        <div class="row row-cols-1 row-cols-lg-2 justify-content-between d-grid d-lg-flex gap-8 gap-lg-0">
             <div class="col d-grid gap-8">
                 <img :src="logo" :alt="VITE_COMPANY_NAME" class="logo">
                 <div class="d-flex align-items-center gap-4">
@@ -20,9 +20,9 @@ const { VITE_COMPANY_NAME } = import.meta.env;
                 </div>
             </div>
             <div class="col d-flex justify-content-end gap-10">
-                <ul class="d-grid gap-4 gap-md-8 list-unstyled mb-0">
+                <ul class="d-grid gap-4 gap-lg-8 list-unstyled mb-0">
                     <li>
-                        <ul class="d-grid d-md-flex align-items-center gap-4 gap-md-12 list-unstyled mb-0">
+                        <ul class="d-grid d-lg-flex align-items-center gap-4 gap-lg-12 list-unstyled mb-0">
                             <li class="d-grid gap-2">
                                 <strong class="d-block text-title">TEL</strong>
                                 <a href="tel:+886-7-1234567" class="link-neutral-0 text-decoration-none fw-normal">+886-7-1234567</a>
@@ -34,13 +34,13 @@ const { VITE_COMPANY_NAME } = import.meta.env;
                         </ul>
                     </li>
                     <li>
-                        <ul class="d-grid d-md-flex align-items-center gap-4 gap-md-12 list-unstyled mb-0">
+                        <ul class="d-grid d-lg-flex align-items-center gap-4 gap-lg-12 list-unstyled mb-0">
                             <li class="d-grid gap-2">
                                 <strong class="d-block text-title">FAX</strong>
                                 <span>+886-7-1234567</span>
                             </li>
                             <li class="d-grid gap-2">
-                                <strong class="d-block text-title">MAIL</strong>
+                                <strong class="d-block text-title">WEB</strong>
                                 <a href="www.elhhexschool.com.tw" class="link-neutral-0 text-decoration-none fw-normal">www.elhhexschool.com.tw</a>
                             </li>
                         </ul>
@@ -48,9 +48,9 @@ const { VITE_COMPANY_NAME } = import.meta.env;
                 </ul>
             </div>
         </div>
-        <div class="row row-cols-1 row-cols-md-2 d-grid d-md-flex gap-4 gap-md-0">
+        <div class="row row-cols-1 row-cols-lg-2 d-grid d-lg-flex gap-4 gap-lg-0">
             <div class="col">806023 台灣高雄市新興區六角路123號</div>
-            <div class="col text-md-end">© 享樂酒店 2023 All Rights Reserved.</div>
+            <div class="col text-lg-end">© 享樂酒店 2023 All Rights Reserved.</div>
         </div>
     </div>
 </template>
